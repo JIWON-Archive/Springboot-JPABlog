2a70b38b3603af7c8e6c00a3cb1a0cbac520daa1
jiwon <lavender_814@naver.com>
2024-05-12 20:39:40 +0900

    spring security

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.cos.blog.config;
 
+import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.config.auth.PrincipalDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,12 +18,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity	// 시큐리티 필터가 등록이 된다. 시큐리티 필터 추가 = 스프링 시큐리티가 활성화 되어있는데 어떤 설정을 해당 파일에서 하겠다.
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	@Autowired
+	private PrincipalDetailService principalDetailService;
 	@Bean // IoC가 되요 함수가 리턴하는 값을 스프링이 관리한다.
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
+	// 시큐리티가 대신 로그인해주는데 password를 가로채기 하는 데
+	// 해당 password가 뭘로 해쉬가 되어 회원가입이 되는 지 알아야
+	// 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -29,8 +43,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll()
 				.anyRequest()		// 아닌 모든 요청은
 				.authenticated() 	// 인증이 되야한다.
+				// 인증이 되지 않은 모든 요청은 무조건 loginForm으로 온다. 24/05/12
 			.and()	//인증이 필요한 페이지로 요청이 오면
 				.formLogin()	
-				.loginPage("/auth/loginForm");	// customizing한 페이지로 간다.
+				.loginPage("/auth/loginForm")	// customizing한 페이지로 간다.
+				.loginProcessingUrl("/auth/loginProc")
+				.defaultSuccessUrl("/"); // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인해준다.
 	}
 }
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -4,13 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cos.blog.model.User1;
 
+import java.util.Optional;
+
 // JpaRepository는 User1 테이블을 관리, PK는 Integer
 // 기본적인 CRUD 모든 함수를 들고 있다.
 // jsp로 치면 DAO
 // 자동으로 bean 등록이 된다. 스프링이 메모리에 띄워준다.
 //@Repository 생략이 가능한다.
 public interface UserRepository extends JpaRepository<User1, Integer> {
-
+    // SELECT * FROM user WHERE username = 1? (첫번째 파라미터);
+    Optional<User1> findByUsername(String username);
 }
 // 로그인 시 사용 1번
 // JPA Naming 쿼리 전략
